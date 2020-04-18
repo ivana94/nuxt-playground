508b3494507dad58560fd28ab746f28ff3477888
ivana94 <ivmatij@gmail.com>
2020-04-18 17:52:36 +0200

    playing with nuxt basics

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app">
-    <nav-bar />
-    <nuxt />
+  <div class="nav">
+    <nuxt-link to="/" class="brand">
+      Real World Events
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/">
+        List
+      </nuxt-link>
+      <nuxt-link to="/create">
+        Create
+      </nuxt-link>
+    </nav>
   </div>
 </template>
-
-<script>
-import NavBar from "~/components/NavBar.vue";
-export default {
-  components: {
-    NavBar,
-  },
-};
-</script>
 
 <style>
 html {
--- pages/create.vue
+++ pages/create.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>events</h1>
+    <h1>create</h1>
   </div>
 </template>
