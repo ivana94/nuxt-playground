c2205767808d60fe16ee5de974f0ca2a029099d2
ivana94 <ivmatij@gmail.com>
2020-04-18 20:20:27 +0200

    add meta tags for seo

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,25 @@
     <h1>create</h1>
   </div>
 </template>
+
+<script>
+import NavBar from "~/components/NavBar.vue";
+export default {
+  head() {
+    // used by vue-meta
+    return {
+      title: "Create an Event",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Where you can create an events",
+        },
+      ],
+    };
+  },
+  components: {
+    NavBar,
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,14 @@
     <h1>events</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    // used by vue-meta
+    return {
+      title: "Event Listning - Real World Events",
+    };
+  },
+};
+</script>
